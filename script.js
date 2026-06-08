b5b6399f0b71e09ab3977eb0859280757710da1a
alohaslushyy32-source <alohaslushyy32@gmail.com>
2026-06-08 01:43:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thanks! We will contact you soon.");
+  this.reset();
+});
 
+// SCROLL REVEAL
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
